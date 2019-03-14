f00a85cafce3c1859a6312db9f12c2bf20ec07d5
jayolee <jayoungl@andrew.cmu.edu>
2019-03-14 15:11:26 -0400

    Updates

---
+++ precache-manifest.19e491b60292e283ce741e2f41710e03.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fbbdf47646eeaffcf23e0bf8a343b6c",
+    "url": "/pagurus/static/media/camera.6fbbdf47.svg"
+  },
+  {
+    "revision": "b73ce6b927c533ca41a3",
+    "url": "/pagurus/static/css/main.03a105ed.chunk.css"
+  },
+  {
+    "revision": "c09856cf3326f78cc03f",
+    "url": "/pagurus/static/js/runtime~main.fc19ff34.js"
+  },
+  {
+    "revision": "749a673694297fb0ec2f",
+    "url": "/pagurus/static/js/2.4d86e239.chunk.js"
+  },
+  {
+    "revision": "612ce1bbe2a30db63dc8a5c8f13d7973",
+    "url": "/pagurus/static/media/mentee.612ce1bb.svg"
+  },
+  {
+    "revision": "c3a84ec48dfc7be984ca2fa8774cff91",
+    "url": "/pagurus/static/media/mentor_prof.c3a84ec4.svg"
+  },
+  {
+    "revision": "11440591e83913e9ec934f61efb899e9",
+    "url": "/pagurus/static/media/longbg.11440591.svg"
+  },
+  {
+    "revision": "b73ce6b927c533ca41a3",
+    "url": "/pagurus/static/js/main.eed65843.chunk.js"
+  },
+  {
+    "revision": "194656974906208b25f5cb66afed5a33",
+    "url": "/pagurus/static/media/mountain.19465697.svg"
+  },
+  {
+    "revision": "d7d66c8eeffbc08f04dc31557767b06c",
+    "url": "/pagurus/static/media/logo_color.d7d66c8e.svg"
+  },
+  {
+    "revision": "fab30209acc8f574479cd6913330bb65",
+    "url": "/pagurus/static/media/logo_gradient.fab30209.svg"
+  },
+  {
+    "revision": "69de5fdc6823521cede1caa5c1c19f67",
+    "url": "/pagurus/static/media/bg1.69de5fdc.png"
+  },
+  {
+    "revision": "046bbf5e8ae686583d16ce7427bfc148",
+    "url": "/pagurus/static/media/settings.046bbf5e.svg"
+  },
+  {
+    "revision": "b0c46272c1d4a0774cf4ca9450667dab",
+    "url": "/pagurus/static/media/mentor.b0c46272.svg"
+  },
+  {
+    "revision": "07f0aab4f0ca7fd1f0e5530d21de1ff9",
+    "url": "/pagurus/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pagurus/precache-manifest.0bb5e0f19b4db534c3e6f9bacf6b95d3.js"
+  "/pagurus/precache-manifest.19e491b60292e283ce741e2f41710e03.js"
 );
 
 workbox.clientsClaim();
